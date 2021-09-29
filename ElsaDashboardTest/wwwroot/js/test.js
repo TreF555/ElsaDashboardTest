cd710a6444762114a4289ea90a8b05621ea36770
Devyatkin Oleg <odevyatkin@elar.ru>
2021-09-29 22:11:45 +0300

    Add parseJson function

--- ElsaDashboardTest/wwwroot/js/test.js
+++ ElsaDashboardTest/wwwroot/js/test.js
@@ -12,12 +12,15 @@
             return;
 
         const props = activityModel.properties || [];
+        //console.log(props);
         const syntax = "Json"//;SyntaxNames.Json;
 
         // Get the value stored in the `PossibleOutcomes` property.
         // Keep in mind that activity properties are stored in a dictionary of expressions, keyed by syntax.
         const possibleOutcomes = props.find(x => x.name == 'PossibleOutcomes') || { expressions: { 'Json': '[]' }, syntax: syntax };
         const expression = possibleOutcomes.expressions[syntax] || [];
+
+        console.log(expression);
 
         // Set the outcomes property of the `context` argument to the list of possible outcomes.
         // Due to the way expressions are serialized depending on the syntax used, we need to check for a couple of formats in order to get the array.
@@ -27,3 +30,16 @@
     eventBus.on('activity-design-displaying', this.onActivityDesignDisplaying);
 
 }
+
+export function parseJson(json) {
+    if (!json)
+        return null;
+
+    try {
+        return JSON.parse(json);
+    } catch (e) {
+        console.warn(`Error parsing JSON: ${e}`);
+    }
+    return undefined;
+}
+
