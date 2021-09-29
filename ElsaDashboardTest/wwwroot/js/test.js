4000c9227ecc2adbbc8bd27b9c4d6eb7c6911fbe
Devyatkin Oleg <odevyatkin@elar.ru>
2021-09-29 21:48:47 +0300

    Change eventBus on event

--- ElsaDashboardTest/wwwroot/js/test.js
+++ ElsaDashboardTest/wwwroot/js/test.js
@@ -1,6 +1,6 @@
 ﻿export default function RandomizeOutcomePlugin(elsaStudio) {
     const { eventBus } = elsaStudio;
-    eventBus.on('activity-design-displaying', this.onActivityDesignDisplaying);
+    
 
     this.onActivityDesignDisplaying = context => {
         //alert('test');
@@ -12,7 +12,7 @@
             return;
 
         const props = activityModel.properties || [];
-        const syntax = SyntaxNames.Json;
+        const syntax = "Json"//;SyntaxNames.Json;
 
         // Get the value stored in the `PossibleOutcomes` property.
         // Keep in mind that activity properties are stored in a dictionary of expressions, keyed by syntax.
@@ -23,4 +23,7 @@
         // Due to the way expressions are serialized depending on the syntax used, we need to check for a couple of formats in order to get the array.
         context.outcomes = !!expression['$values'] ? expression['$values'] : Array.isArray(expression) ? expression : parseJson(expression) || [];
     }
+
+    eventBus.on('activity-design-displaying', this.onActivityDesignDisplaying);
+
 }
